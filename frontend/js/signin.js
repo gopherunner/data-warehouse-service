4ffff60b5afbabc77992ad91480e8dfa82bf3782
German Anders <ganders@despegar.com>
2020-12-09 16:32:34 -0300

    add body encoded and add session storage on token

--- frontend/js/signin.js
+++ frontend/js/signin.js
@@ -1,34 +1,40 @@
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const signInBtn = document.getElementById("btn");
+const signInBtn = document.getElementById("login-btn");
 const notification = document.getElementById("notification");
+let loginHeaders = new Headers();
+loginHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
 const APP_SERVER = "http://127.0.0.1:3000";
 const APP_CLIENT = "http://127.0.0.1:5500/project-data-warehouse/frontend";
 
-signInBtn.addEventListener("click", () => {
-    signIn();
-});
+signInBtn.addEventListener("click", (event) => {
+    event.preventDefault();
 
-function signIn() {
-    console.log("[DEBUG] - User: " + emailInput.value + ", " + passwordInput.value);
-    fetch(`${APP_SERVER}/users/login`, {
+    let loginUrl = APP_SERVER + '/users/login';
+    let bodyEncoded = new URLSearchParams();
+
+    bodyEncoded.append("email", emailInput.value);
+    bodyEncoded.append("password", passwordInput.value);
+
+    let requestBody = {
         method: 'POST',
-        body: `{ "email": "${emailInput.value}", "password": "${passwordInput.value}"}`,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(resp => {
-        if(resp.status == 200) {
-            resp.json().then((data) => {
-                console.log("[DEBUG] => Token: ", data.token);
-                sessionStorage.setItem("token", data.token);
-            });
-            console.log("[DEBUG] => Login Successfully!");
-            location.href = "home.html";
-        } else {
-            notification.innerHTML = "Email or password is not valid!"
-            console.log("[ERROR] error: " + resp.json);
-        }
-    });
-};
+        headers: loginHeaders,
+        body: bodyEncoded,
+    };
+
+    fetch(loginUrl, requestBody)
+        .then(response => {
+            if (response.status == 200 || response.status == 201) {
+                response.json().then((result) => {
+                    let token = result.token;
+                    console.log("[DEBUG] => Token: " + token);
+                    sessionStorage.setItem("token", token);
+                });
+                location.href = "home.html";
+            } else {
+                notification.innerHTML = "Email or password is not valid!";
+                console.log("[ERROR] error: " + response.json());
+            }
+        });
+});
