1613577de1914ae888ca19ea626f8536b53f0932
German Anders <ganders@despegar.com>
2021-01-24 20:14:38 -0300

    update companies schema for mongo

--- backend/models/companies.js
+++ backend/models/companies.js
@@ -1,58 +1,27 @@
-const Sequelize = require('sequelize');
-const db = require('../db/connection');
+const { Schema, model } = require('mongoose');
 
-const Company = db.sequelize.define('companies', {
-        company_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        company_name: {
-            type: Sequelize.STRING,
-            unique: true,
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                    args: true,
-                    msg: "The company name can't be empty",
-                },
-            },
-        },
-        company_address: {
-            type: Sequelize.STRING,
-            allowNull: false,
-            validate: {
-                notEmpty: {
-                    args: true,
-                    msg: "The company address can't be empty",
-                },
-            },
-        },
-        company_country_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        company_city_id: {
-            type: Sequelize.INTEGER,
-            allowNull: false,
-        },
-        company_email: {
-            type: Sequelize.STRING,
-            unique: true,
-            allowNull: false,
-            validate: {
-                isEmail: {
-                    args: true,
-                    msg: "Enter a valid email",
-                },
-            },
-        },
-        company_phone: {
-            type: Sequelize.STRING,
-            allowNull: false,
-        },
+const companySchema = new Schema({
+    name: {
+        type: String,
+        unique: true
     },
-);
+    address: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    phone: {
+        type: String,
+        unique: true
+    },
+    city: [{
+        type: Schema.Types.ObjectId,
+        ref: 'City'
+    }]
+});
 
-module.exports = { Sequelize, Company };
+const company = model('Company', companySchema);
+
+module.exports = { companySchema: company };
