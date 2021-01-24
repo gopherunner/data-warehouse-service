1daaaa36116ce33265148cae02d9028ec8d42b92
German Anders <ganders@despegar.com>
2021-01-24 20:12:45 -0300

    update endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,53 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const helmet = require('helmet');
 require('dotenv').config();
+
 
 // Import endpoints
 const users = require('./endpoints/users');
+const companies = require('./endpoints/companies');
+const contacts = require('./endpoints/contacts');
+const regions = require('./endpoints/regions');
+const countries = require('./endpoints/countries');
+const cities = require('./endpoints/cities');
 
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:" + process.env.APP_PORT
+    origin: '*'
 };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use(helmet.permittedCrossDomainPolicies({permittedPolicies: "by-content-type"}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if ("OPTIONS" == req.method) {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
 
 // Init database
-const db = require("./db/connection");
+const { db, dbURI } = require('./db/connection');
+const bulkInitData = require('./db/bulkInitData');
 
-console.log("[INFO] Connecting to the Database (" + db.db + ") on port " + db.port + "...")
-db.init()
-.then(async () => {
-    console.log("[INFO] Connection established!");
-    
-    // Start server only if DB was started correctly 
-    app.listen(process.env.APP_PORT,() => {
-        console.log("[INFO] Starting DataWarehouse Mkt API Server, listening on port " + process.env.APP_PORT);
+console.log("[INFO] Connecting to the Database...");
+db.once('open', ()=> {
+    console.log("[INFO] Database connected to " + dbURI);
+    // Bulk init entries
+    bulkInitData();
+
+    app.listen(process.env.APP_PORT, () => {
+        console.log("[INFO] Starting Datawarehouse Mkt API Server, listening on port " + process.env.APP_PORT);
     });
-
-}).catch((err) => {
-	console.log("[ERROR] While trying to connect to the Database", err);
 });
+db.on('error', console.error.bind(console, 'Connection error'));
 
 // Main endpoint
 app.get('/', (req, res) => {
@@ -40,3 +56,13 @@ app.get('/', (req, res) => {
 
 // Users endpoint
 app.use('/users', users);
+// Companies endpoint
+app.use('/companies', companies);
+// Contacts endpoint
+app.use('/contacts', contacts);
+// Regions endpoint
+app.use('/regions', regions);
+// Countries endpoint
+app.use('/countries', countries);
+// Cities endpoint
+app.use('/cities', cities);
